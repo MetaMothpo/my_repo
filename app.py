aa6e1f6568ac6918db17dc28094f23456a9309ab
MetaMothapo <metamakobedi@gmail.com>
2024-07-06 09:37:40 +0200

    mmm

---
+++ app.py
@@ -0,0 +1,23 @@
+import nest_asyncio
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+# Aljjjjlow FahstAPI to run in thehhhh Jupyter notebogggok
+nest_asyncio.apply()
+
+app = FastAPI()
+
+class InputModel(BaseModel):
+    x: float
+
+class OutputModel(BaseModel):
+    y: float
+
+@app.post("/compute", response_model=OutputModel)
+def compute(input_model: InputModel):
+    x = input_model.x
+    y = 2 * x + 3
+    return OutputModel(y=y)
+
+
